a6c69732a6437ff191ae7ea4a9d13e1936367d0c
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-06-02 18:46:39 -0400

    Updated database backup for ui defaults

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-python manage.py dumpdata data_cube_ui dc_algorithm custom_mosaic_tool coastal_change fractional_cover ndvi_anomaly slip tsm water_detection > db_backups/init_database.json
+python manage.py dumpdata dc_algorithm custom_mosaic_tool coastal_change fractional_cover ndvi_anomaly slip tsm water_detection > db_backups/init_database.json
