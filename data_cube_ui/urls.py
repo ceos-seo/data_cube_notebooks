e946080cfefd36462e5d98effa1ebdad4080342b
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-06-03 16:22:17 -0400

    fixed imports in project root

--- data_cube_ui/urls.py
+++ data_cube_ui/urls.py
@@ -37,7 +37,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
