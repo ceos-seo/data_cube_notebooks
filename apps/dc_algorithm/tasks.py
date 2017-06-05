c81feda8b5fbf31e87aad9f8aeba35c4a6bc93f3
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-06-05 17:01:29 -0400

    Fixed an issue with the cache clearing task - userhistory model needed to have .objects as the class itself has no filter method.

--- apps/dc_algorithm/tasks.py
+++ apps/dc_algorithm/tasks.py
@@ -15,14 +15,14 @@ from .models import Application
     ignore_result=True)
 def clear_cache():
     _apps = Application.objects.all()
-    time_threshold = datetime.now() - timedelta(days=7)
+    time_threshold = datetime.now() - timedelta(days=2)
     for app in _apps:
         camel_case = "".join(x.title() for x in app.pk.split('_'))
         task_model = apps.get_model(".".join([app.pk, camel_case + "Task"]))
         history_model = apps.get_model(".".join([app.pk, "UserHistory"]))
         tasks = task_model.objects.filter(execution_start__lt=time_threshold)
         for task in tasks:
-            history_model.filter(task_id=task.pk).delete()
+            history_model.objects.filter(task_id=task.pk).delete()
             shutil.rmtree(task.get_result_path())
             task.delete()
     print("Cache Cleared.")
