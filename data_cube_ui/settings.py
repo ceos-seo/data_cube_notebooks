a34db597f882b8fa03717e4a6f5a32febf723343
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-08-04 11:46:41 -0400

    Start of the pixel plotting utility

--- data_cube_ui/settings.py
+++ data_cube_ui/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
     'apps.cloud_coverage',
     'apps.nazeer_chlorophyll',
     'apps.watanabe_chlorophyll',
+    'apps.pixel_plotting',
     'apps.dc_algorithm',
     'apps.pages',
     'apps.accounts',
