6eb99f6707fb50bcf24d319d0d34331d21578127
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-08-30 17:14:36 -0400

    Updated automation scripts to dump the correct data, updated database backup.

--- scripts/dump_data.sh
+++ scripts/dump_data.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-python manage.py dumpdata dc_algorithm custom_mosaic_tool coastal_change fractional_cover ndvi_anomaly slip tsm water_detection cloud_coverage urbanization nazeer_chlorophyll watanabe_chlorophyll spectral_indices > db_backups/init_database.json
+python manage.py dumpdata dc_algorithm custom_mosaic_tool coastal_change fractional_cover ndvi_anomaly slip tsm water_detection cloud_coverage urbanization spectral_indices > db_backups/init_database.json
--- scripts/initial_migrations.sh
+++ scripts/initial_migrations.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 python manage.py makemigrations
-python manage.py makemigrations {dc_algorithm,accounts,coastal_change,custom_mosaic_tool,fractional_cover,ndvi_anomaly,slip,task_manager,tsm,water_detection,cloud_coverage,urbanization,nazeer_chlorophyll,watanabe_chlorophyll,spectral_indices,data_cube_manager}
+python manage.py makemigrations {dc_algorithm,accounts,coastal_change,custom_mosaic_tool,fractional_cover,ndvi_anomaly,slip,task_manager,tsm,water_detection,cloud_coverage,urbanization,spectral_indices,data_cube_manager}
 python manage.py migrate
 
 python manage.py loaddata db_backups/init_database.json
