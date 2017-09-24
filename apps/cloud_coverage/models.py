dfe287a36db061efabbc1b0c307e1484a4e76770
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-09-24 18:31:11 -0400

    updated db dump again

--- apps/cloud_coverage/models.py
+++ apps/cloud_coverage/models.py
@@ -105,7 +105,7 @@ class Query(BaseQuery):
 
         """
 
-        def clear_percentage(dataset_in, clean_mask, intermediate_product=None, nodata=-9999):
+        def clear_percentage(dataset_in, clean_mask, intermediate_product=None, no_data=-9999):
             """Calculate the total number of clear pixels and the total number of pixels
 
             Args:
