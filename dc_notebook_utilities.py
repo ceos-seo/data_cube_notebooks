b87cd311e5f67cef75689e6fd9f4ae36beb71b2c
Otto Wagner <owagner@bigdata-node3.internal.ama-inc.com>
2018-05-16 10:37:27 -0400

    Fix rgb plotting function in notebook utilities.

--- dc_notebook_utilities.py
+++ dc_notebook_utilities.py
@@ -201,28 +201,57 @@ def show_map_extents(min_lon, max_lon, min_lat, max_lat):
     plt.show()
 
 
-def rgb(dataset, at_index = 0, bands = ['red', 'green', 'blue'], paint_on_mask = [], figsize = (20,20)):
-    rgb = np.stack([dataset[bands[0]], dataset[bands[1]], dataset[bands[2]]], axis = -1)
-    max_possible = 3500
-    rgb = rgb.astype(np.float32)
-    rgb[rgb<0] = 0
-    rgb[rgb > max_possible] = max_possible
-    rgb *= 255.0/rgb.max()
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+from time import time
+import numpy as np
 
-    rgb = rgb.astype(int)
-    rgb = rgb.astype(np.float32)
-    rgb = 255-rgb
+
+# Change the bands (RGB) here if you want other false color combinations
+def rgb(dataset,
+        at_index = 0,
+        bands = ['red', 'green', 'blue'],
+        paint_on_mask = [],
+        max_possible = 3500,
+        width = 10
+       ):
+
+    def aspect_ratio_helper(x,y, fixed_width = 20):
+        width = fixed_width
+        height = y * (fixed_width / x)
+        return (width, height)
     
-    rgb[rgb > 254] = 254
-    rgb[rgb < 1]   = 1
+    ### < Dataset to RGB Format, needs float values between 0-1 
+    rgb = np.stack([dataset[bands[0]],
+                    dataset[bands[1]],
+                    dataset[bands[2]]], axis = -1).astype(np.int16)
     
+    rgb[rgb<0] = 0    
+    rgb[rgb > max_possible] = max_possible # Filter out saturation points at arbitrarily defined max_possible value
+    
+    rgb = rgb.astype(float)
+    rgb *= 1 / np.max(rgb)
+    ### > 
+    
+    ### < takes a T/F mask, apply a color to T areas  
     for mask, color in paint_on_mask:        
-        rgb[mask] = np.array([256,256,256]) - np.array(color).astype(np.int16)
+        rgb[mask] = np.array(color)/ 255.0
+    ### > 
     
-    plt.figure(figsize = figsize)
+    
+    fig, ax = plt.subplots(figsize = aspect_ratio_helper(*rgb.shape[:2], fixed_width = width))
+
+    lat_formatter = FuncFormatter(lambda x, pos: round(dataset.latitude.values[pos] ,4) )
+    lon_formatter = FuncFormatter(lambda x, pos: round(dataset.longitude.values[pos],4) )
+
+    plt.ylabel("Latitude")
+    ax.yaxis.set_major_formatter(lat_formatter)
+    plt.xlabel("Longitude")
+    ax.xaxis.set_major_formatter(lon_formatter)
+   
     if 'time' in dataset:
         plt.imshow((rgb[at_index]))
     else:
-        plt.imshow(rgb)
-
-
+        plt.imshow(rgb)  
+    
+    plt.show()
