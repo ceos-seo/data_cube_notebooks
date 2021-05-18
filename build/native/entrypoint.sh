8a1935bc4bf9c741be8d920af2a831e524d9ff5c
John Rattz <john.c.rattz@ama-inc.com>
2021-05-17 22:11:26 -0400

    More build patches (mostly for security - avoid leaking AWS credentials).

--- build/native/entrypoint.sh
+++ build/native/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Run startup initialization.
-CONTAINER_STARTED="/tmp/container_started"
+CONTAINER_STARTED="/etc/container_started"
 if [ ! -e $CONTAINER_STARTED ]; then
-    bash build/native/odc_conf.sh
+    cd $BUILD_DIR
+    bash native/odc_conf.sh
     touch $CONTAINER_STARTED
+    cd $WORKDIR
+    /bin/tini -s -- jupyter notebook --allow-root --ip='0.0.0.0' --NotebookApp.token='$NBK_SERVER_PASSWORD'
 fi
 
-/bin/tini -- "$@"
+exec "$@"
--- build/native/odc_conf.sh
+++ build/native/odc_conf.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-mkdir -p config && echo "\
-[datacube] \n\
-db_hostname: ${ODC_DB_HOSTNAME} \n\
-db_database: ${ODC_DB_DATABASE} \n\
-db_username: ${ODC_DB_USER} \n\
-db_password: ${ODC_DB_PASSWORD} \n" > config/datacube.conf
+mkdir -p config && echo """\
+[datacube]
+db_hostname: ${ODC_DB_HOSTNAME}
+db_database: ${ODC_DB_DATABASE}
+db_username: ${ODC_DB_USER}
+db_password: ${ODC_DB_PASSWORD}""" > config/datacube.conf
 cp config/datacube.conf /etc/datacube.conf
-export DATACUBE_CONFIG_PATH=${BUILD_DIR}/config/datacube.conf
+export DATACUBE_CONFIG_PATH=/etc/datacube.conf
