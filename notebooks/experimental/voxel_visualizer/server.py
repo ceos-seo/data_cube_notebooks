bc3561b164698651ae0c943614466fd71177efdb
John Rattz <john.c.rattz@ama-inc.com>
2021-08-12 13:59:14 -0400

    Allow the voxel visualizer webserver to be run on a specified (not pre-defined) port.

--- notebooks/experimental/voxel_visualizer/server.py
+++ notebooks/experimental/voxel_visualizer/server.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import uvicorn
@@ -7,27 +9,29 @@ import pathlib
 app = FastAPI()
 
 app.add_middleware(CORSMiddleware,
-	allow_origins = ["*"],
-	allow_methods = ["*"],
-	allow_headers = ["*"],
-	allow_credentials = True)
+    allow_origins = ["*"],
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+    allow_credentials = True)
 
 
 mountable = [["/static", "./"]]
 
 for api_route, directory_path in mountable:
-	print("")
+    print("")
 
-	directory_path = pathlib.Path(directory_path).resolve()
+    directory_path = pathlib.Path(directory_path).resolve()
 
-	print(f"Mounting:{directory_path} on {api_route}")
-	app.mount(api_route, StaticFiles(directory=directory_path), name= "static")
+    print(f"Mounting:{directory_path} on {api_route}")
+    app.mount(api_route, StaticFiles(directory=directory_path), name= "static")
 
-	for x in directory_path.glob("./*"):
-		print(":::",x)
+    for x in directory_path.glob("./*"):
+        print(":::",x)
+        
+print('port:', os.environ['VOXEL_VISUALIZER_PORT'])
 
 uvicorn.run(app,
-			port = 8082,
-			host = "0.0.0.0"
-			)
+            port = os.environ['VOXEL_VISUALIZER_PORT'],
+            host = "0.0.0.0"
+            )
 
